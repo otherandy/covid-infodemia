fa29c1b742ca28fc13c17549d59a1e86b92910e7
Andrés Herrera <18634968+otherandy@users.noreply.github.com>
2021-05-20 00:38:36 -0700

    Removed styling

--- public/etiquetado.php
+++ public/etiquetado.php
@@ -2,39 +2,30 @@
 <html lang="es">
 
 <head>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Pruebas de Etiquetado</title>
 </head>
 <?php
 require "php-partials/db-conn.php";
 $id_resumen = "1";
 ?>
 
-<div class="container">
-  <a href="#">Noticias Covid1 ejemplo</a>
-  <?php
-  $consulta = "select * from etiquetas_de_resumen a,etiquetas b where resumenes_id='" . $id_resumen . "' and etiquetas_id=b.id";
-  if ($ejecutar = $conn->query($consulta)) :
-    while ($fila = $ejecutar->fetch_assoc()) :
-  ?>
-      <span class="badge badge-info"><?= $fila['nombre'] ?></span>
-  <?php
-    endwhile;
-  endif;
-  ?>
-  <style>
-    .edit {
-      background: none;
-      border: none;
-      margin: 0;
-      padding: 0;
-      cursor: pointer;
-    }
-  </style>
-  <button class="edit">
-    <span class="badge badge-success">Edit</span>
-  </button>
+<body>
+  <div>
+    <a>Noticias Covid1 ejemplo</a>
+    <?php
+    $consulta = "select * from etiquetas_de_resumen a,etiquetas b where resumenes_id='" . $id_resumen . "' and etiquetas_id=b.id";
+    if ($ejecutar = $conn->query($consulta)) :
+      while ($fila = $ejecutar->fetch_assoc()) : ?>
+        <span><?= $fila['nombre'] ?></span>
+    <?php
+      endwhile;
+    endif; ?>
+    <button>
+      <span>Edit</span>
+    </button>
+  </div>
+</body>
 
-</div>
+</html>
--- public/tag-search.php
+++ public/tag-search.php
@@ -1,66 +1,46 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Buscar en repositorio</title>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </head>
 <?php
 require "php-partials/db-conn.php";
 ?>
 
 <body>
-  <div class="container">
-    <h1>Buscador Covid Repositorio-tags</h1>
-    <form id="form" action="tag-search.php" method="POST">
-      <div class="form-group">
-        <input type="text" id="search" name="search" class="form-control" placeholder="search">
-      </div>
-      <button type="submit" class="button.btn.btn-danger btn-block">buscar</button>
-      <div class="form-group">
-      </div>
-    </form>
-  </div>
-  <div class="container">
-    <?php
-    if ($_POST) :
+  <h1>Buscador Covid Repositorio-tags</h1>
+  <form action="tag-search.php" method="POST">
+    <input type="text" name="search" placeholder="search">
+    <button type="submit">buscar</button>
+  </form>
+  <?php
+  if ($_POST) : ?>
+    <div>
+      <?php
       $consulta = "select * from etiquetas_de_resumen a,etiquetas b,resumenes c where b.nombre like '" . $_POST['search'] . "' and etiquetas_id=b.id";
       if ($ejecutar = $conn->query($consulta)) :
-        while ($fila = $ejecutar->fetch_assoc()) :
-    ?>
-          <a href="#"><?= $fila['texto'] ?></a>
+        while ($fila = $ejecutar->fetch_assoc()) : ?>
+          <a><?= $fila['texto'] ?></a>
           <?php
           $consulta2 = "select * from etiquetas_de_resumen a,etiquetas b where resumenes_id='" . $fila['resumenes_id'] . "' and etiquetas_id=b.id";
           if ($ejecutar2 = $conn->query($consulta2)) :
             while ($fila2 = $ejecutar2->fetch_assoc()) : ?>
-              <span class="badge badge-info"><?= $fila2['nombre'] ?></span>
+              <span><?= $fila2['nombre'] ?></span>
           <?php
             endwhile;
-          endif;
-          ?>
-          <style>
-            .edit {
-              background: none;
-              border: none;
-              margin: 0;
-              padding: 0;
-              cursor: pointer;
-            }
-          </style>
-          <button class="edit">
-            <span class="badge badge-success">Edit</span>
+          endif; ?>
+          <button>
+            <span>Edit</span>
           </button>
-
-    <?php
+      <?php
         endwhile;
-      endif;
-    endif;
-    ?>
-  </div>
-
+      endif; ?>
+    </div>
+  <?php
+  endif; ?>
 </body>
 
 </html>
