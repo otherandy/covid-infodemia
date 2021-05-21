2c6140a674cf747724b3417b8ef072ba8461146e
moonslayers <84060574+moonslayers@users.noreply.github.com>
2021-05-21 11:55:28 -0700

    Add files via upload

---
+++ public/registro.php
@@ -0,0 +1,68 @@
+<?php
+session_start();
+$message="";
+require 'php-partials/db-conn.php';
+
+/*if (isset($_SESSION['tipo'])) {
+    header('Location: index.php');
+}*/
+if($_POST){
+    $usuario=$_POST['usuario'];
+    $pass=$_POST['contraseña'];
+    $pass2=$_POST['contraseña2'];
+    $correo=$_POST['correo'];
+    $tipo="usuario";
+    if($pass!=$pass2){
+        $message="<h4>Las contraseñas no coinciden</h4>";
+    }else{
+        
+        $pass = password_hash($_POST['contraseña'], PASSWORD_BCRYPT);//encriptamos la contraseña
+        $sql="INSERT INTO `usuarios`(`id_usuario`, `nombre`, `password`, `email`, `tipo`) VALUES (NULL,'".$usuario."','".$pass."','".$correo."','".$tipo."')";
+        $query = mysqli_query($conn,$sql);
+        if($query){
+            $message="<h4>Usuario creado con exito</h4>";
+            $_SESSION['usuario']=$usuario;
+            $_SESSION['tipo']=$tipo;
+
+        }else{
+            $message="Error al guardar: ".$sql;
+        }
+
+    }
+
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <title>Registro Alumno</title>
+</head>
+
+
+
+<body class="form-v10">
+    <div class="page-content">
+        <div class="form-v10-content">
+            <?=$message?>
+            <form class="form-detail" action="registro.php" method="POST" id="myform">
+                <input type="text" name="usuario" placeholder="nombre usuario">
+                <input type="pass" name="contraseña" placeholder="contraseña">
+                <input type="pass" name="contraseña2" placeholder=" confirme contraseña">
+                <input type="pass" name="correo" placeholder="correo electronico">
+
+                    <div class="container">
+                        <input type="submit" onclick="alert(Registro Satisfactorio)" name="register" class="register"
+                            value="Registrar">
+                    </div>
+
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
