aa3e3ef81b5b6dc8252364e74d7ca14a233d915b
Andrés Herrera <18634968+otherandy@users.noreply.github.com>
2021-05-20 21:23:40 -0700

    Removed styling

--- public/index.js
+++ public/index.js
@@ -1,25 +1,23 @@
-$("#form").submit(function(e) {
-    e.preventDefault()
+$("#form").submit(function (e) {
+  e.preventDefault();
 
-    var query = $("#search").val()
-    let result = ''
-    var API_KEY = 'c937939c823eba91c8bc0b71d54bc6a5'
+  var query = $("#search").val();
+  let result = "";
+  var API_KEY = "c937939c823eba91c8bc0b71d54bc6a5";
 
-    var url = 'http://api.serpstack.com/search?access_key=' + API_KEY + '&type=web&query=' + query
-    console.log(url)
+  var url =
+    "http://api.serpstack.com/search?access_key=" +
+    API_KEY +
+    "&type=web&query=" +
+    query;
+  console.log(url);
 
-    $.get(url, function(data) {
-        console.log(data)
-        $("#result").empty()
-        data.organic_results.forEach(res => {
-            result = `
-            
-            <h1>${res.title}</h1><br><a href="${res.url}">${res.url}</a>
-            <p>${res.snippet}</p>
-
-            `
-            $("#result").append(result)
-        });
-
-    })
-})
+  $.get(url, function (data) {
+    console.log(data);
+    $("#result").empty();
+    data.organic_results.forEach((res) => {
+      result = `<h1>${res.title}</h1><br><a href="${res.url}">${res.url}</a><p>${res.snippet}</p>`;
+      $("#result").append(result);
+    });
+  });
+});
