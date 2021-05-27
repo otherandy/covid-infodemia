7d74f30314c66b5184118bb5d4450ff77af9a88f
moonslayers <84060574+moonslayers@users.noreply.github.com>
2021-05-27 00:20:41 -0700

    new table resumenes-cambios for admin check in app

--- tests/test.sql
+++ tests/test.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: May 21, 2021 at 08:57 PM
+-- Generation Time: May 27, 2021 at 09:19 AM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 7.3.27
 
@@ -29,11 +29,12 @@ USE `covid`;
 -- Table structure for table `etiquetas`
 --
 
-CREATE TABLE `etiquetas` (
-  `id` int(10) UNSIGNED NOT NULL,
+CREATE TABLE IF NOT EXISTS `etiquetas` (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `nombre` varchar(45) NOT NULL,
-  `fecha_creacion` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `fecha_creacion` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `etiquetas`
@@ -49,9 +50,12 @@ INSERT INTO `etiquetas` (`id`, `nombre`, `fecha_creacion`) VALUES
 -- Table structure for table `etiquetas_de_resumen`
 --
 
-CREATE TABLE `etiquetas_de_resumen` (
+CREATE TABLE IF NOT EXISTS `etiquetas_de_resumen` (
   `resumenes_id` int(10) UNSIGNED NOT NULL,
-  `etiquetas_id` int(10) UNSIGNED NOT NULL
+  `etiquetas_id` int(10) UNSIGNED NOT NULL,
+  PRIMARY KEY (`resumenes_id`,`etiquetas_id`),
+  KEY `resumenes_id` (`resumenes_id`),
+  KEY `etiquetas_id` (`etiquetas_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -59,7 +63,8 @@ CREATE TABLE `etiquetas_de_resumen` (
 --
 
 INSERT INTO `etiquetas_de_resumen` (`resumenes_id`, `etiquetas_id`) VALUES
-(1, 1);
+(1, 1),
+(1, 2);
 
 -- --------------------------------------------------------
 
@@ -67,12 +72,13 @@ INSERT INTO `etiquetas_de_resumen` (`resumenes_id`, `etiquetas_id`) VALUES
 -- Table structure for table `resumenes`
 --
 
-CREATE TABLE `resumenes` (
-  `id` int(10) UNSIGNED NOT NULL,
+CREATE TABLE IF NOT EXISTS `resumenes` (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `texto` mediumtext NOT NULL,
   `original` varchar(255) NOT NULL,
-  `fecha_creacion` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `fecha_creacion` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `resumenes`
@@ -84,16 +90,42 @@ INSERT INTO `resumenes` (`id`, `texto`, `original`, `fecha_creacion`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `resumenes-cambios`
+--
+
+CREATE TABLE IF NOT EXISTS `resumenes-cambios` (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id_resumen` int(11) NOT NULL,
+  `texto` mediumtext NOT NULL,
+  `original` varchar(255) NOT NULL,
+  `fecha_creacion` datetime NOT NULL,
+  `nombre_usuario` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `resumenes-cambios`
+--
+
+INSERT INTO `resumenes-cambios` (`id`, `id_resumen`, `texto`, `original`, `fecha_creacion`, `nombre_usuario`) VALUES
+(2, 1, 'asda', 'asd', '2021-05-27 22:20:59', 'anonimo'),
+(4, 1, 'Noticia sobre vacunas', 'data actualizada', '2021-05-18 11:34:04', 'usuario1'),
+(5, 1, 'Noticia sobre vacunas', 'data actualizada', '2021-05-18 11:34:04', 'usuario1');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `usuarios`
 --
 
-CREATE TABLE `usuarios` (
-  `id_usuario` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `id_usuario` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `tipo` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `tipo` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_usuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `usuarios`
@@ -102,58 +134,6 @@ CREATE TABLE `usuarios` (
 INSERT INTO `usuarios` (`id_usuario`, `nombre`, `password`, `email`, `tipo`) VALUES
 (1, 'usuario1', '$2y$10$bFo9BEyhcxTug2.3uPgvGOz9BHUbS4/d43ayUWh0GOZyfpcAy.mf6', 'prueba@hotmail.com', 'usuario'),
 (2, 'admin', '$2y$10$Y8GrWSDmDweanTBIx6vHS.zi4ucIaUbGgb4dFzl5/7X0S1w.Cwfr.', 'admin@admin.com', 'admin');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `etiquetas`
---
-ALTER TABLE `etiquetas`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `etiquetas_de_resumen`
---
-ALTER TABLE `etiquetas_de_resumen`
-  ADD PRIMARY KEY (`resumenes_id`,`etiquetas_id`),
-  ADD KEY `resumenes_id` (`resumenes_id`),
-  ADD KEY `etiquetas_id` (`etiquetas_id`);
-
---
--- Indexes for table `resumenes`
---
-ALTER TABLE `resumenes`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id_usuario`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `etiquetas`
---
-ALTER TABLE `etiquetas`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `resumenes`
---
-ALTER TABLE `resumenes`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `usuarios`
---
-ALTER TABLE `usuarios`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
