0cf85bab1124331e6efcbef834293ddc4c797121
moonslayers <84060574+moonslayers@users.noreply.github.com>
2021-06-02 12:05:46 -0700

    graphic layer implemented

--- public/login.php
+++ public/login.php
@@ -40,30 +40,117 @@ if (!empty($_POST['usuario']) && !empty($_POST['pass'])) {
 
 <!DOCTYPE html>
 <html>
-
 <head>
+    <meta charset="utf-8">
+    <title>Iniciar sesión</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
 
-    <title>Registro Alumno</title>
+    <style type="text/css" media="screen">
+        #Sesion{
+            background-image: linear-gradient(to top, #ffffff, #eef0ff, #d3e4ff, #afdaff, #7fd2ff);
+            padding-top:30px ;
+            margin-bottom: 20px;
+        }
+        #textoCentrado{
+            text-align: center;
+        }
+        #inputPassword{
+            border-radius: 10px;
+        }
+        #exampleFormControlInput1{
+            border-radius: 10px;
+        }
+        #separacionCH{
+            margin-top: 5px;
+            margin-bottom: 10px;
+
+        }
+        #separacionM{
+            margin-top: 10px;
+            margin-bottom: 15px;
+        }
+        #separacionG{
+            margin-top: 25px;
+            margin-bottom: 30px;
+        }
+        #problemas{
+            font-size: 14px;
+        }
+        #sugerencias{
+            font-size: 18px;
+        }
+        #boton{
+            margin: auto;
+            display: block;
+        }
+        #objetivo{
+                cursor:pointer;
+                font-size: 16px;
+            }
+    </style>
+    
+    <script type="text/javascript">
+        var paginaObjetivos="acercaDe.html";
+
+        function objetivos(){
+            location.href=paginaObjetivos;
+        }
+        
+    </script>
 </head>
+<body class="row justify-content-center">
+    <div class="col-12" id="Sesion">
+        <h1 class="display-6" id="textoCentrado">Inicio de sesión</h1>
+    </div>
+    <div class="col-12" id="separacionCH"></div>
+    <div class="col-3"></div>
+    <div class="col-6">
+        <div class="shadow p-3 mb-5 bg-body rounded" id="fondoInicioSesion">
+            <div class="row justify-content-center" >
+                <div class="col-1"></div>
+                <div class="col-10">
+                    <form action="login.php" method="POST" class="row justify-content-center">
+                        <label for="inputPassword" class="col-sm-3 col-form-label">Usuario</label>
+                        <div class="col-sm-9">
+                         <input type="text" name='usuario' class="form-control" id="exampleFormControlInput1" placeholder="">
+                        </div>
+
+                        <div class="col-12" id="separacionM"></div>
+
+                        <label for="inputPassword" class="col-sm-3 col-form-label">Contraseña</label>
+                        <div class="col-sm-9">
+                         <input name="pass" type="password" class="form-control" id="inputPassword">
+                        </div>
+                        <div class="col-12" id="separacionG">
+                            <button type="submit" class="btn btn-outline-primary" id="boton">Iniciar sesión</button>
+                        </div>
 
 
-<h5><a href="index.php">inicio</a></h5>
+                        </div>
+                        <div class="col-12">
+                            <p>
+                                <h1 class="display-6" id="objetivo" onclick="objetivos()">Nuestro objetivo</h1>
+                              <a class="display-6" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample" id="problemas">
+                                ¿Problemas con el inicio de sesión?
+                              </a>
 
-<body class="form-v10">
-    <div class="page-content">
-        <div class="form-v10-content">
-            <?= $message ?>
-            <form class="form-detail" action="login.php" method="POST" id="myform">
-                <input type="text" name="usuario" placeholder="nombre usuario">
-                <input type="password" name="pass" placeholder="contraseña">
-
-                <div class="container">
-                    <input type="submit" onclick="alert(Registro Satisfactorio)" name="register" class="register" value="login">
-                </div>
-
-            </form>
+                            </p>
+                            <div class="collapse" id="collapseExample">
+                                <div class="card card-body">
+                                    <h1 class="display-6" id="sugerencias">contacte al administrador para solucionar su inicio de sesion</h1>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                <div class="col-1"></div>
+            </div>
         </div>
     </div>
-</body>
+    <div class="col-3"></div>   
+    <div class="col-12" style="height: 100%;"></div>
 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+</body>
 </html>
