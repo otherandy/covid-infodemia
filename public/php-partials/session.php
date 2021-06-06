8cbcabe074d12f5def1e9bde51dfea1b218af4c7
Nestor Flores <74647712+Nestorini101@users.noreply.github.com>
2021-06-06 01:57:04 -0700

    Text aligns to the right

--- public/php-partials/session.php
+++ public/php-partials/session.php
@@ -2,11 +2,11 @@
 session_start();
 
 if (isset($_SESSION['nombre']) and $_SESSION['tipo'] != "admin") {
-	echo '<h5>' . $_SESSION['nombre'] . ' <a href="logout.php">Cerrar Sesion</a>.</h5>';
+	echo '<h5 style="text-align:right;">' . $_SESSION['nombre'] . ' <a href="logout.php">Cerrar Sesion</a> </h5>';
 }
 if (isset($_SESSION['nombre']) and $_SESSION['tipo'] == "admin") {
-	echo '<h5>' . $_SESSION['nombre'] . '  <a href="requests.php">resumenes actualizados</a>  <a href="logout.php">Cerrar Sesion</a>.</h5>';
+	echo '<h5 style="text-align:right;"><a href="requests.php">' . $_SESSION['nombre'] . '  </a>  <a href="logout.php">Cerrar Sesion</a> </h5>';
 }
 if (!isset($_SESSION['nombre'])) {
-	echo '<h5><a href="login.php">Iniciar Sesion</a>   <a href="registro.php">Registrate</a></h5>';
+	echo '<h5 style="text-align:right;"><a href="login.php">Iniciar Sesion</a>   <a href="registro.php">Registrate</a> </h5>';
 }
