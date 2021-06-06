227d720927af0345de164f77cb17b2d2c262580d
Nestor Flores <74647712+Nestorini101@users.noreply.github.com>
2021-06-06 01:55:28 -0700

    Applied graphical layer
    index antes era el buscador y la pagina de resultados todo en uno, pero ahora se separo en dos, index es el buscador y search te muestra los resultados.

--- public/index.php
+++ public/index.php
@@ -1,130 +1,147 @@
 <?php require "php-partials/db-conn.php"; ?>
-<!DOCTYPE html>
-<html lang="es">
+<!doctype html>
+<html lang="en">
+	 <head>
+	    <!-- Required meta tags -->
+	    <meta charset="utf-8">
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-  <style type="text/css">
-    #zonaImagen {
-      margin-top: 50px;
-      margin-bottom: 30px;
-      height: 200px;
+	    <!-- Bootstrap CSS -->
+	    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+	    <style type="text/css">
+	    	#ZonaImagen{
+	    		display: block;
+	    		margin: auto;
+	    	}
+	    	#padding10{
+	    		padding: 12px;
+	    		border-radius: 25px;
+	    		text-align: center;
+	    	}
+	    	#padding30{
+	    		padding-top: 50px;
+	    		text-align: center;
+	    	}
+	    	#encabezado{
+	    		text-align: center;
+	    		font-weight: 400;
+				font-size: 24px;
+	    	}
+	    	img {
+				margin: auto;
+				display: block;
+			}
+			#zonaDeFrase{
+				margin-top: 20px;
+				margin-bottom: 50px;
+			}
+			#iniciarsesion{
+				color: rgba(36, 113, 163,.8);
+				font-size: 16px;
+			}
+	    	#objetivo{
+				color: rgb(21, 67, 96);
+				font-weight: 400;
+				font-size: 16px;
+			}
+			a{
+				cursor:pointer;
+			}
+			
+		</style>
+	    <title>Buscador</title>
 
-    }
+	    <script type="text/javascript">
+	    	var imagen="https://webimages.iadb.org/Drupal_pantheon/2020-08/1-%281%29.png?VersionId=XywK8tvqUHQXXCyaEBVKRe13Alhvq4Bl";
+	    	var paginaInformacion="acercaDe.html";
+	    	var palabraABuscar="";
+	    	var enlaceAResultados="search.php";
 
-    .padding10 {
-      padding: 12px;
-      border-radius: 25px;
-      text-align: center;
-    }
+	    	function cambioLogo(){
+				document.getElementById('logo').src=imagen;
+	    	}
+	    	function enlaces(){
+				document.getElementById('objetivo').href=paginaInformacion;
+	    	}
+	    	function buscar(event){
+	    			//le agregue algunas funciones basicas pero es necesario autenticar el
+	    			//texto recibido para solo aceptar cadenas alfanumericas
 
-    .padding11 {
-      padding: 5px;
-      border-radius: 117px;
-      text-align: center;
-    }
+	    		var tecla= event.key;
+	    		if(tecla=="Enter"&&palabraABuscar==""){
+	    			alert("Por favor introduzca al menos una palabra para realizar la busqueda");
+	    		}
+	    		else if(tecla=="Backspace" && palabraABuscar.length>0 ){
+	    				palabraABuscar=palabraABuscar.substring(0, palabraABuscar.length - 1);
+	    		}
+	    		else if(tecla=="Backspace" && palabraABuscar.length==0 ){
+	    				//no hace nada
+	    		}
+	    		else if(tecla=="Enter"){
+					location.href = enlaceAResultados + '?q=' + palabraABuscar;
+	    			//ir a la pagina de resultados de busqueda
+	    		}
+	    		else {
+	    				palabraABuscar+=tecla;
+	    		}
+	    	}
 
-    .margen15 {
-      margin: 15px;
-    }
+	    	function isMobile(){
+		    	return (
+		        	(navigator.userAgent.match(/Android/i)) ||
+		        	(navigator.userAgent.match(/webOS/i)) ||
+		        	(navigator.userAgent.match(/iPhone/i)) ||
+		        	(navigator.userAgent.match(/iPod/i)) ||
+		        	(navigator.userAgent.match(/iPad/i)) ||
+		        	(navigator.userAgent.match(/BlackBerry/i))
+		    	);
+			}
+			
+	    	function datosDePagina(){
+	    		cambioLogo();
+	    		enlaces();
 
-    img {
-      margin-left: 70px;
-    }
-  </style>
-  <title>Buscador</title>
-</head>
+	    	}
+			function nuestroObjetivo(){
+				location.href=paginaInformacion;
+			}
+			
+	    </script>
+	 </head>
+  	<body class="row justify-content-center" onload="datosDePagina()">
+	<?php require "php-partials/session.php"; ?>
+		<div class="row justify-content-center">
+			<div class="col-2" id="padding30"></div>
+			<div class="col-12" id="Zonaimagen">
+				<img src="" height="180" width="180" id="logo">
+			</div>
+			
+			<div class="col-2"></div>
+			<div class="col-8" id="zonaDeFrase">
+				<h6 class="display-6" id="encabezado">Juntos para evitar la desinformación</h6>
+			</div>
+			<div class="col-2"></div>
+			<div class="col-3"></div>
+			<div class="col-6">
+			    <input type="text" class="form-control" placeholder="Introduce la busqueda y oprime la tecla enter" aria-label="Recipient's username" aria-describedby="basic-addon2" id="padding10" onkeyup="buscar(event)">	
+			</div>
+			<div class="col-3"></div>
+			<div class="col-3"></div>
+			<div class="col-6" id="padding30">
+				<div class="row justify-content-center">
+			   		<div class="col-5" id="movil1">
+			   			<a class="display-6" id="objetivo" href="" onclick="nuestroObjetivo()">Nuestro objetivo</a>
+			   		</div>
+			   	</div>
+			</div>
+			<div class="col-3"></div>
 
-<body>
-  <?php require "php-partials/session.php"; ?>
+			</div>
+  		
+			
+		</div>
 
-  <?php
-  if (!$_GET) : ?>
 
-    <div class="container">
-      <div id="zonaImagen" class="row">
-        <div class="col-4">
-        </div>
-        <div class="col-4">
-          <img src="https://webimages.iadb.org/Drupal_pantheon/2020-08/1-%281%29.png?VersionId=XywK8tvqUHQXXCyaEBVKRe13Alhvq4Bl" height="150" width="150">
-          <h6>Juntos para evitar la desinformación</h6>
-        </div>
-      </div>
-      <div class="col-4">
-      </div>
-    </div>
-
-    <form action="index.php" method="GET">
-      <div class="row gy-5">
-        <div class="col-2"></div>
-        <div class="col-7">
-          <div class="input-group mb-3">
-            <input type="text" name="search" class="form-control padding10" placeholder="Introduce la busqueda">
-          </div>
-        </div>
-        <div class="col-3">
-          <button type="submit" class="btn btn-primary me-md-2 padding10">BUSCAR</button>
-        </div>
-      </div>
-    </form>
-    </div>
-
-  <?php
-  else : ?>
-    <div class="container">
-      <div class="margen15"></div>
-      <form action="index.php" method="GET">
-        <div class="row gy-5">
-          <div class="col-2"></div>
-          <div class="col-6">
-            <div class="input-group mb-3">
-              <input type="text" name="search" class="form-control padding11" placeholder="Introduce la busqueda" value="<?= $_GET ? $_GET['search'] : "" ?>">
-            </div>
-          </div>
-          <div class="col-4">
-            <button type="submit" class="btn btn-primary me-md-2 padding11">BUSCAR</button>
-          </div>
-        </div>
-      </form>
-    </div>
-
-    <div class="container">
-      <div class="margen15"></div>
-      <div class="row gy-5">
-        <div class="col-1"></div>
-
-        <div id="resutados" class="col-10">
-          <div class="pahe-header">
-            <h6 id="Titulo" style="text-align: center;"></h6>
-          </div>
-          <?php
-          $consulta = "select * from etiquetas_de_resumen,etiquetas e,resumenes r where e.nombre like '" . $_GET['search'] . "' and etiquetas_id=e.id and resumenes_id=r.id";
-          if ($ejecutar = $conn->query($consulta)) :
-            $rows = mysqli_num_rows($ejecutar);
-            echo "<p>resultados encontrados: ".$rows."</p>";
-            while ($fila = $ejecutar->fetch_assoc()) : ?>
-              <a href="preview.php?id=<?= $fila['resumenes_id']; ?>"><?= $fila['texto'] ?> <p><?= substr($fila['fecha_creacion'], 0, -9) ?></p> </a>
-              <?php
-              $consulta2 = "select * from etiquetas_de_resumen,etiquetas e where resumenes_id='" . $fila['resumenes_id'] . "' and etiquetas_id=e.id";
-              if ($ejecutar2 = $conn->query($consulta2)) :
-                while ($fila2 = $ejecutar2->fetch_assoc()) : ?>
-                  <span><?= $fila2['nombre'] ?></span>
-          <?php
-                endwhile;
-              endif;
-            endwhile;
-          endif; ?>
-        </div>
-
-        <div id="mostrarResultados" class="col-1"></div>
-      </div>
-    </div>
-  <?php
-  endif; ?>
-
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
-</body>
-
+    	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+  </body>
 </html>
